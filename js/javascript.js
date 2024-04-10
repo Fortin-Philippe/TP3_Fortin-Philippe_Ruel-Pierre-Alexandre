242243b990c4b74dcfdf42b9c110e113f6543a5c
P-A-Ruel <pirue001@edu.cegepgarneau.ca>
2024-04-10 17:10:28 -0400

    Commit fin formulaire de base + logique de verification

--- js/javascript.js
+++ js/javascript.js
@@ -3,27 +3,27 @@
 
 
 /* ### DÉBUT - VOS DONNÉES DE DÉPART (QUESTIONS/RÉPONSES) */
-let donnees = 
-[
-	{
-		"question":"À quoi sert un aria-label?",
-		"réponses":[
-		"Ajouter du contenu textuel sur une balise pour aider les lecteurs d'écran",
-		"À rien", 
-		"Je ne sais pas"
-		], 
-		"réponse":0
-	},
-	{
-		"question":"HTML vient de :",
-		"réponses":[
-			"Hyper Typo Meta Lol",
-			"Hypertext markup language", 
-			"Je ne sais pas"
-		], 
-		"réponse":1
-	}
-];
+let donnees =
+	[
+		{
+			"question": "À quoi sert un aria-label?",
+			"réponses": [
+				"Ajouter du contenu textuel sur une balise pour aider les lecteurs d'écran",
+				"À rien",
+				"Je ne sais pas"
+			],
+			"réponse": 0
+		},
+		{
+			"question": "HTML vient de :",
+			"réponses": [
+				"Hyper Typo Meta Lol",
+				"Hypertext markup language",
+				"Je ne sais pas"
+			],
+			"réponse": 1
+		}
+	];
 
 /* Exemple d'utilisation
 // Question1
@@ -39,99 +39,129 @@ donnees[1].réponse;        -> 1
 
 
 /* ### DÉBUT - SECTION VARIABLES GLOBALES */
-   // Vos variables globales
+// Vos variables globales
 
 /* ### FIN - SECTION VARIABLES GLOBALES */
 
 
 
 /* ### DÉBUT - SECTION FONCTIONS */
-   // Vos fonctions
-   // ..
-   // ..
+// Vos fonctions
+// ..
+// ..
 
 
-function gererBoutonCommencer()
-{
+function gererBoutonCommencer() {
 	// Retirer cette alert une fois le bouton complété
-	alert("gererBoutonCommencer");
+	
 
-	let formulaireEstValide = false;
+	
 
 	//1) Valider chaque champ du formulaire selon les critères de l'énoncé
 	let PrenomValide = false;
 	let NomValide = false;
-    let MotDePasseValide = false;
+	let MotDePasseValide = false;
+	let confirmationmotdepassevalide = false;
 	let emailValide = false;
+	let formulaireEstValide = false;
+
 
 	let prenom = document.getElementById("prenom").value;
-	let nom = document.getElementById("nom").value;
-    let motDePasse = document.getElementById("motdepasse").value;
-	let confirmationmotdepasse = document.getElementById("confirmationemail").value;
+	let nom = document.getElementById("Nom").value;
+	let motDePasse = document.getElementById("mdp").value;
+	let confirmationmotdepasse = document.getElementById("mdpconfirmation").value;
 	let email = document.getElementById("email").value;
+	const emailParts = email.split("@");
 
-	if(prenom == "")
-	{
-		document.getElementById("msgErreurprenom").textContent = "erreur";
+	// Validation du prenom
+	if (prenom == "") {
+		document.getElementById("msgErreurprenom").textContent = "Le champs ne peut etre vide";
 	}
-	else
-	{
+	else {
 		PrenomValide = true;
 		document.getElementById("msgErreurprenom").textContent = "";
 	}
-
-	if(nom == "")
-	{
-		document.getElementById("msgErreurnom").textContent = "erreur";
+	
+	// Validation du nom
+	if (nom == "") {
+		document.getElementById("msgErreurnom").textContent = "Le champs ne peut etre vide";
 	}
-	else
-	{
-		PrenomValide = true;
+	else {
+		NomValide = true;
 		document.getElementById("msgErreurnom").textContent = "";
 	}
-	if(motDePasse == ""){
-		if(motDePasse.length >= 8){
+
+	// Validation du mdp
+	if (motDePasse != "") {
+		if (motDePasse.length >= 8) {
 			document.getElementById("msgErreurmdp").textContent = "";
-			motDePasse = true;
+			MotDePasseValide = true;
 		}
-		else{
-			document.getElementById("msgErreurmdp").textContent = "erreur";
+		else {
+			document.getElementById("msgErreurmdp").textContent = "Mot de passe invalide";
 		}
+	}
+	else {
+		document.getElementById("msgErreurmdp").textContent = "Le champs ne peut etre vide";
+	}
+
+    // Validation de la confirmation
+	if(confirmationmotdepasse != motDePasse){
+		document.getElementById("msgErreurconfirmationmdp").textContent = "N'est pas identique";
 	}
 	else{
-		document.getElementById("msgErreurmdp").textContent = "erreur";
+		document.getElementById("msgErreurconfirmationmdp").textContent = "";
+		confirmationmotdepassevalide = true;
+
 	}
+
+	// Validation du email
+	if(email == "" || emailParts.length !== 2 ){
+		document.getElementById("msgErreuremail").textContent = "Email invalide";
+	}
+	else{
+		emailValide = true;
+	}
+
+	// validation du formulaire
+	if(PrenomValide && NomValide && emailValide && confirmationmotdepassevalide && MotDePasseValide){
+		alert("entree reussite");
+		formulaireEstValide = true;
+	}
+	
+
+
+
+
 	// 2) Rendu ici, on devrait savoir si tous les champs sont valides ou si au moins 1 champ est invalide
-	if(formulaireEstValide)
-	{
+	if (formulaireEstValide) {
 		// Si le formulaire est valide, on peut maintenant supprimer en javascript la balise form (incluant tout son contenu)
 		// et commencer à créer en javascript la première question, ses choix de réponses, ainsi que le bouton "Soumettre", qui sera utiliser avancer dans le quiz
 		// d'une question à l'autre
 		// Attention de conserver les informations du formulaire (avant de supprimer celui-ci) dans des variables javascripts car vous en aurez besoin
 		// tout au long du quiz (ex. le prénom et le nom)
 	}
-	else
-	{	
+	else {
 		// Si le formulaire est invalide, on ne poursuit pas vers le quiz, donc on ne fait rien : l'utilisateur doit corriger
 		// le ou les champs invalides.
 	}
-	
-} 
+
+}
 /* ### FIN - SECTION FONCTIONS */
 
 
 
 function initialisation() {
 
-   //Ici, je ne fais qu'affiche le contenu de l'objet donnees vers la console
-   //du navigateur pour des fins de "débug". Vous pouvez le conserver ou non.
-   for (const key in donnees) {
-	console.log(donnees[key]);
-   }
+	//Ici, je ne fais qu'affiche le contenu de l'objet donnees vers la console
+	//du navigateur pour des fins de "débug". Vous pouvez le conserver ou non.
+	for (const key in donnees) {
+		console.log(donnees[key]);
+	}
 
-   document.getElementById("btnCommencer").addEventListener("click",gererBoutonCommencer);
+	document.getElementById("btnCommencer").addEventListener("click", gererBoutonCommencer);
 
-   // TODO...
+	// TODO...
 }
 
 addEventListener('load', initialisation, false);
