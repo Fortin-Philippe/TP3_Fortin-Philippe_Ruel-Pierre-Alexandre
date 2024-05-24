e0bba7641b2f073bc89a299f7fb35dfff65e65f9
Fortin-Philippe <159812164+Fortin-Philippe@users.noreply.github.com>
2024-05-24 13:45:49 -0400

    Ajout du Line manque condition et tableaux de resultats.

--- js/javascript.js
+++ js/javascript.js
@@ -174,19 +174,19 @@ function gererBoutonCommencer() {
 //Fonction qui génère le quiz et qui retire le formulaire de départ et l'image de départ.
 //Fonction qui génère le quiz et qui retire le formulaire de départ et l'image de départ.
 function GenererQuiz() {
-    const formulaireDepart = document.getElementById("formulaireDepart");
-    const imgDepart = document.getElementById("imgDepart");
+	const formulaireDepart = document.getElementById("formulaireDepart");
+	const imgDepart = document.getElementById("imgDepart");
 
-    if (formulaireDepart) {
-        formulaireDepart.remove();
-    }
+	if (formulaireDepart) {
+		formulaireDepart.remove();
+	}
 
-    if (imgDepart) {
-        imgDepart.remove();
-    }
-    
-    SalutationsHeader();
-    AffichageQuestion(0);
+	if (imgDepart) {
+		imgDepart.remove();
+	}
+
+	SalutationsHeader();
+	AffichageQuestion(0);
 }
 
 //Fonction qui fait référence à une divSalutations dans le html dans laquelle il y a p qui salut l'utilisateur avec son nom et prénom.
@@ -208,7 +208,7 @@ function AffichageQuestion(numeroQuestion) {
 	let totalQuestion = donnees.length;
 	//Variable qui réfère à la section main en html.
 	const main = document.getElementById("main");
-	
+
 	const progress = ((numeroQuestion + 1) / totalQuestion) * 100;
 	progressBar.style.width = `${progress}%`;
 	progressBar.setAttribute('aria-valuenow', progress);
@@ -303,7 +303,7 @@ function GererSuivant(prochaineQuestion) {
 	}
 	//Sinon on affiche le résultat.
 	else {
-		
+
 		AffichageQuestion(prochaineQuestion);
 	}
 
@@ -312,76 +312,76 @@ function GererSuivant(prochaineQuestion) {
 
 // Fonction qui affiche le résultat des réponses du questionnaire. 
 function AfficherResultat() {
-    // Masquer la barre de progression
-    const divProgress = document.getElementById("progressbarID");
-    divProgress.style.display = "none";
+	// Masquer la barre de progression
+	const divProgress = document.getElementById("progressbarID");
+	divProgress.style.display = "none";
 
-    // Effacer le contenu du questionnaire et afficher les résultats
-    const questionnaire = document.getElementById("questionnaire");
-    questionnaire.innerHTML = "";
-    const divAvancementQuestion = document.getElementById("avancementQuestion");
-    divAvancementQuestion.innerHTML = "";
+	// Effacer le contenu du questionnaire et afficher les résultats
+	const questionnaire = document.getElementById("questionnaire");
+	questionnaire.innerHTML = "";
+	const divAvancementQuestion = document.getElementById("avancementQuestion");
+	divAvancementQuestion.innerHTML = "";
 
-    let nombreDeBonneReponses = ObtenirNombreDeBonneReponse();
-    const pourcentageResultat = (nombreDeBonneReponses / donnees.length) * 100;
-    const pourcentageFormate = pourcentageResultat.toFixed(2);
+	let nombreDeBonneReponses = ObtenirNombreDeBonneReponse();
+	const pourcentageResultat = (nombreDeBonneReponses / donnees.length) * 100;
+	const pourcentageFormate = pourcentageResultat.toFixed(2);
 
-    let divPositionTexte = document.getElementById("divPositionnement");
-    if (!divPositionTexte) {
-        divPositionTexte = document.createElement("div");
-        divPositionTexte.id = "divPositionnement";
-        document.body.appendChild(divPositionTexte);  // Ajouter l'élément au body ou à un conteneur spécifique
-    }
-    divPositionTexte.innerHTML = "";  // Réinitialiser le contenu
-    divPositionTexte.classList.add("row");
+	let divPositionTexte = document.getElementById("divPositionnement");
+	if (!divPositionTexte) {
+		divPositionTexte = document.createElement("div");
+		divPositionTexte.id = "divPositionnement";
+		document.body.appendChild(divPositionTexte);  // Ajouter l'élément au body ou à un conteneur spécifique
+	}
+	divPositionTexte.innerHTML = "";  // Réinitialiser le contenu
+	divPositionTexte.classList.add("row");
 
-    let divTextResultat = document.createElement("div");
-    divTextResultat.id = "divTextResultat";
-    divTextResultat.classList.add("col-md-6", "col-sm-12", "padding-0");
+	let divTextResultat = document.createElement("div");
+	divTextResultat.id = "divTextResultat";
+	divTextResultat.classList.add("col-md-6", "col-sm-12", "padding-0");
 
-    const textResultatNom = document.createElement("p");
-    textResultatNom.id = "textResultatom";
-    textResultatNom.textContent = `Résultat pour ${prenomUtilisateur} ${nomUtilisateur} :`;
+	const textResultatNom = document.createElement("p");
+	textResultatNom.id = "textResultatom";
+	textResultatNom.textContent = `Résultat pour ${prenomUtilisateur} ${nomUtilisateur} :`;
 
-    const textPourcentage = document.createElement("p");
-    textPourcentage.textContent = `${pourcentageFormate}%`;
+	const textPourcentage = document.createElement("p");
+	textPourcentage.textContent = `${pourcentageFormate}%`;
 
-    divPositionTexte.appendChild(divTextResultat);
-    divTextResultat.appendChild(textResultatNom);
-    divTextResultat.appendChild(textPourcentage);
+	divPositionTexte.appendChild(divTextResultat);
+	divTextResultat.appendChild(textResultatNom);
+	divTextResultat.appendChild(textPourcentage);
 
-    let divImage = document.createElement("div");
-    divImage.id = "divImage";
-    divImage.classList.add("col-md-6", "col-sm-12");
-    divPositionTexte.appendChild(divImage);
+	let divImage = document.createElement("div");
+	divImage.id = "divImage";
+	divImage.classList.add("col-md-6", "col-sm-12");
+	divPositionTexte.appendChild(divImage);
 
-    if (pourcentageFormate >= 60) {
-        let imageReussite = document.createElement("img");
-        imageReussite.src = "img/reussite.jpg";
-        imageReussite.alt = "Image de réussite.";
-        divImage.appendChild(imageReussite);
-        imageReussite.classList.add("img-fluid");
-    } else {
-        let imageEchec = document.createElement("img");
-        imageEchec.src = "img/echec.jpg";
-        imageEchec.alt = "Image d'échec.";
-        divImage.appendChild(imageEchec);
-        imageEchec.classList.add("img-fluid");
-    }
+	if (pourcentageFormate >= 60) {
+		let imageReussite = document.createElement("img");
+		imageReussite.src = "img/reussite.jpg";
+		imageReussite.alt = "Image de réussite.";
+		divImage.appendChild(imageReussite);
+		imageReussite.classList.add("img-fluid");
+	} else {
+		let imageEchec = document.createElement("img");
+		imageEchec.src = "img/echec.jpg";
+		imageEchec.alt = "Image d'échec.";
+		divImage.appendChild(imageEchec);
+		imageEchec.classList.add("img-fluid");
+	}
 
-    const btnRecommencer = document.createElement("button");
-    btnRecommencer.classList.add("btn", "btn-primary");
-    btnRecommencer.textContent = "Recommencer";
-    btnRecommencer.addEventListener("click", recommencerQuiz);
-    divTextResultat.appendChild(btnRecommencer);
+	const btnRecommencer = document.createElement("button");
+	btnRecommencer.classList.add("btn", "btn-primary");
+	btnRecommencer.textContent = "Recommencer";
+	btnRecommencer.addEventListener("click", recommencerQuiz);
+	divTextResultat.appendChild(btnRecommencer);
 
 	//Affichage du doughnut
 	let canvaDoughnut = document.getElementById("myChartDoughnut");
 	canvaDoughnut.style.display = "block";
 	canvaDoughnut.style.width = "40%";
 	canvaDoughnut.style.padding = "20px";
-	
-	
+
+
 	let nombreDeMauvaiseReponse = donnees.length - nombreDeBonneReponses;
 	let chartDoughnut = new Chart(canvaDoughnut, {
 		type: "doughnut",
@@ -393,7 +393,51 @@ function AfficherResultat() {
 			}]
 		}
 	});
-	//Affichage du diagramme à ligne.
+
+
+function recommencerQuiz() {
+
+	reponsesUtilisateur = [];
+
+	// Obtenir les références des éléments à vider
+	const quizContainer = document.getElementById("quiz-container");
+	const resultContainer = document.getElementById("result-container");
+	const divSalutations = document.getElementById("divSalutations");
+	const avancementQuestion = document.getElementById("avancementQuestion");
+	const divImage = document.getElementById("divImage");
+	const divTextResultat = document.getElementById("divTextResultat");
+	const divProgress = document.getElementById("progressbarID");
+	const canvaDoughnut = document.getElementById("myChartDoughnut");
+	// Vider le contenu textuel des éléments sans supprimer les enfants
+	if (quizContainer) {
+		quizContainer.textContent = "";
+	}
+	if (resultContainer) {
+		resultContainer.textContent = "";
+	}
+	if (divSalutations) {
+		divSalutations.textContent = "";
+	}
+	if (avancementQuestion) {
+		avancementQuestion.textContent = "";
+	}
+	if (divImage) {
+		divImage.textContent = "";
+	}
+	if (divTextResultat) {
+		divTextResultat.textContent = "";
+	}
+	canvaDoughnut.style.display = "none";
+
+
+	// Réinitialiser la barre de progression
+	if (divProgress) {
+		divProgress.style.width = "0%";
+		divProgress.style.display = "none"; // Masquer la barre de progression
+	}
+
+	// Générer à nouveau le quiz à partir de la première question
+	GenererQuiz();
 	let canvaLine = document.getElementById("myChartLine");
 	canvaLine.style.display = "block";
 	canvaLine.style.width = "100%";
@@ -403,74 +447,25 @@ function AfficherResultat() {
 
 	let chartLine = new Chart(canvaLine, {
 		type: "line",
-		data:{
-			labels:["Resultat précédent", "Résultat actuel"],
-			datasets:[{
+		data: {
+			labels: ["Resultat précédent", "Résultat actuel"],
+			datasets: [{
 				label: "Pourcentage de réussite",
-				data:[35,40],
-				borderColor: "red",
+				data: [35, 40],
+				borderColor: ["red"],
 				fill: false
 			}]
 		},
 		options: {
-		scales:{
-			y:{
-				beginAtZero: true,
-				max: 100
+			scales: {
+				y: {
+					beginAtZero: true,
+					max: 100
+				}
 			}
 		}
-	}
 	});
 }
-
-function recommencerQuiz() {
-    
-    reponsesUtilisateur = [];
-	
-    // Obtenir les références des éléments à vider
-    const quizContainer = document.getElementById("quiz-container");
-    const resultContainer = document.getElementById("result-container");
-    const divSalutations = document.getElementById("divSalutations");
-    const avancementQuestion = document.getElementById("avancementQuestion");
-    const divImage = document.getElementById("divImage");
-    const divTextResultat = document.getElementById("divTextResultat");
-    const canvaLine = document.getElementById("myChartLine");
-    const divProgress = document.getElementById("progressbarID");
-	const canvaDoughnut = document.getElementById("myChartDoughnut");
-    // Vider le contenu textuel des éléments sans supprimer les enfants
-    if (quizContainer) {
-        quizContainer.textContent = "";
-    }
-	
-        canvaLine.style.display="none";
-    
-    if (resultContainer) {
-        resultContainer.textContent = "";
-    }
-    if (divSalutations) {
-        divSalutations.textContent = "";
-    }
-    if (avancementQuestion) {
-        avancementQuestion.textContent = "";
-    }
-	if (divImage) {
-        divImage.textContent = "";
-    }
-    if (divTextResultat) {
-        divTextResultat.textContent = "";
-    }
-    
-        canvaDoughnut.style.display="none";
-    
-
-    // Réinitialiser la barre de progression
-    if (divProgress) {
-        divProgress.style.width = "0%";
-        divProgress.style.display = "none"; // Masquer la barre de progression
-    }
-
-    // Générer à nouveau le quiz à partir de la première question
-    GenererQuiz();
 }
 //Fonction qui enregistre le nombre de bonne réponse. 
 function ObtenirNombreDeBonneReponse() {
